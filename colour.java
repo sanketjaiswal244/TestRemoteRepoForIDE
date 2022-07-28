8a8308af5e308283dc227a0fd6c6af49922686cf
Sanket Jaiswal <sanketjaiswal244@gmail.com>
2022-07-28 12:40:07 +0530

    First

--- colour.java
+++ colour.java
@@ -2,6 +2,6 @@ public class colour
 {
   public static void main (string args[])
   {
-    System.out.println("New file added from  eclipse");
+    System.out.println("New file added from eclipse");
   }
 }
